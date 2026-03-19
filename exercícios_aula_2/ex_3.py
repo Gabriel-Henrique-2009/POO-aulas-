26221d21800723b70cd342239199cfe2e6a8c9c9
Gabriel Henrique <pedrosa.henrique.gabriel@gmail.com>
2026-03-19 13:22:28 -0300

    concertando as coisas

--- exercícios_aula_2/ex_3.py
+++ exercícios_aula_2/ex_3.py
@@ -3,11 +3,10 @@ for i in range(4):
     x = int(input())
     lista.append(x)
     if len(lista) == 4:
-        lista.sort()
-        print(lista[0])
-        print(lista[3])
-        print(f"isso é que é soma {lista[1] + lista[2]}")
-
-
-
-
+        if lista[0] == lista[1] or lista[2] == lista[3] or lista[1] == lista[2] or lista[1] == lista[3] or lista[2] == lista[3]:
+            print("coisas erradas estão acontecendo")
+        else:
+            lista.sort()
+            print(lista[0])
+            print(lista[3])
+            print(f"isso é que é soma {lista[1] + lista[2]}")
