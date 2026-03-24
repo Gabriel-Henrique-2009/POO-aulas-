40fe31caf3cbba24c81a99cbc59ed9cede144cf9
Gabriel Henrique <pedrosa.henrique.gabriel@gmail.com>
2026-03-24 15:49:15 -0300

    melhorando os códigos

--- exercícios_aula_3/area_t.py
+++ exercícios_aula_3/area_t.py
@@ -11,17 +11,23 @@ class triangulo:
 x = triangulo()
 print(x.b, x.h)
 # acessa os valores que estão dentro da classe x (triangulo)
+
 x.b = float(input("Digite a base do triângulo "))
 x.h = float(input("Digite a altura do triângulo "))
+
 print(x.b, x.h)
+
 a = x.calc_area()
 print(f"A área do triângulo é {a:.2f}")
 
 y = triangulo()
 print(y.b, y.h)
+
 y.b = float(input("Digite a base do triângulo "))
 y.h = float(input("Digite a altura do triângulo "))
+
 print(y.b, y.h)
+
 a = y.calc_area()
 print(f"A área do triângulo é {a:.2f}")
 
