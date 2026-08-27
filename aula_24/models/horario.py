5409696233e4e28ecf99ea4e956c542cdedb896f
Gabriel Henrique Pedrosa <pedrosa.henrique.gabriel@gmail.com>
2026-08-27 16:50:52 +0000

    commit

---
+++ aula_24/models/horario.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+class Horario:
+	def __init__(self, id, data):
+		self.set_id(id)
+		self.set_data(data)
+		seld.set_confirmado(False)
+		self.set_id_cliente(0)
+		self.set_id_servico(0)
