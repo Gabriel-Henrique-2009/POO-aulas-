026315814ed72c87a1da5d039cb3903e8841af36
Gabriel <pedrosa.henrique.gabriel@gmail.com>
2026-09-10 13:51:24 -0300

    commitando por causa do streamlit

--- prova_10_setembro/Projeto/index.py
+++ prova_10_setembro/Projeto/index.py
@@ -5,8 +5,8 @@ from template.manterconvenioui import ManterConvenioUI
 class IndexUI:
     @staticmethod
     def main():
-        op = st.sidebar.selectbox("Menu", ["Clientes", "Horários"])
+        op = st.sidebar.selectbox("Menu", ["Clientes", "Convenio"])
         if op == "Clientes": ManterClienteUI.main()
-        if op == "Horários": ManterConvenioUI.main()
+        if op == "Convenio": ManterConvenioUI.main()
 
 IndexUI.main()
